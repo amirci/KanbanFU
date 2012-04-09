67ad29db877769bda05c992b8f365690ade1c250
Amir Barylko <amir@barylko.com>
2012-04-09 07:37:35 -0500

    Implemented missing steps (after removing web_steps) zen-9

--- features/step_definitions/api_steps.rb
+++ features/step_definitions/api_steps.rb
@@ -11,3 +11,11 @@ Given /^I create a new project using the API$/ do
   set_current_project Project.new(attrib)
   post '/projects', :format => :json, 'project' => attrib
 end
+
+When /^I get all the cards for project (\d+) using the API$/ do |project_id|
+  visit "/projects/#{project_id}/cards"
+end
+
+When /^I get card (\d+) for project (\d+) using the API$/ do |card_id, project_id|
+  visit "/projects/#{project_id}/cards/#{card_id}"
+end
--- features/step_definitions/json_steps.rb
+++ features/step_definitions/json_steps.rb
@@ -2,13 +2,13 @@ Then /^the response should be a collection of "(.+)" with:$/ do |root, table|
   table.hashes.each do |row|
     json = row.to_json
     path = "#{root}/#{row['id'].to_i - 1}"
-    Then(%Q{the JSON at "#{path}" should be #{json}})
+    step %Q{the JSON at "#{path}" should be #{json}}
   end
 end
 
 Then /^the response should be a "([^"]+)" with:$/ do |root, table|
   row = table.hashes.first
-  Then(%Q{the JSON at "#{root}" should be #{row.to_json}})
+  step %Q{the JSON at "#{root}" should be #{row.to_json}}
 end
 
 Then /^the response should be "([^"]*)"$/ do |status|
