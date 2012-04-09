164408f1076632ba0984b8eb059e01b9b8388cee
Amir Barylko <amir@barylko.com>
2012-04-09 12:38:21 -0500

    Refactored project controller to use repond_with, added exception handling for record not found to AppController zen-9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  rescue_from ActiveRecord::RecordNotFound, :with => :respond_with_not_found 
+  
+  def current_user
+  end
+  
   private
+    def respond_with_not_found
+      respond_with nil do |format|
+        format.json { render json: nil, :status => :not_found }
+      end
+    end
+
     def render_json(item, options = {})
       options.reverse_merge! :except => [], :error_code => :not_found
-
       status = item ? :ok : options[:error_code]
-      render :json => item,
-        :status => status,
-        :except => options[:except] + [:created_at, :updated_at]
+      render :json => item, :status => status, :except => options[:except]
     end
 
     def render_not_found(message = 'Not Found')
